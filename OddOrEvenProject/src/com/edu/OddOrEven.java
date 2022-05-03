068df902cd6ee4650ee6198f69294e7c0a782778
ELCOT <ELCOT@Lenovo>
2022-05-02 19:48:35 -0700

    odd or even checking project

---
+++ OddOrEvenProject/src/com/edu/OddOrEven.java
@@ -0,0 +1,48 @@
+package com.edu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OddOrEven
+ */
+@WebServlet("/OddOrEven")
+public class OddOrEven extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OddOrEven() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		PrintWriter out=response.getWriter();
+		int number=Integer.parseInt(request.getParameter("num"));
+		if(number%2==0)
+		{
+			out.println("The number is even number");
+		}
+		else
+		{
+			out.println("The number is odd number");
+		}
+		
+	
+	}
+
+}
